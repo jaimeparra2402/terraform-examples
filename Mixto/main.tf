375cc37ebe31873ab681a37863662ae0b75b2692
jaimeparra2402 <jpj451@inlumine.ual.es>
2025-11-10 00:58:59 +0100

    Add GCP username, project, and bucket name

--- Mixto/main.tf
+++ Mixto/main.tf
@@ -1,3 +1,6 @@
 module "Google-Storage" {
-  source              = "./modules/Google-Storage"
+  source          = "./modules/Google-Storage"
+  gcp_username    = "jpj451"
+  gcp_project     = "cc-25-26"
+  gcp_bucket_name = "bucket-jpj451-cc2526"
 }

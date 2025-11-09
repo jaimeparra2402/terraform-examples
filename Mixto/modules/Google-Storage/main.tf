088f57b4e39b3379efcfb015c313a40e559ed410
jaimeparra2402 <jpj451@inlumine.ual.es>
2025-11-10 00:45:05 +0100

    Add Google Storage bucket and image objects
    Created a Google Storage bucket and added two images with public access.

--- Mixto/modules/Google-Storage/main.tf
+++ Mixto/modules/Google-Storage/main.tf
@@ -1,12 +1,40 @@
-# *** YOUR CODE HERE ***
-# Crear un recurso google_storage_bucket para almacenar imágenes en Google Storage
-#
-# Crear en el bucket anteior dos recursos google_storage_bucket_object
-# (uno para cada imagen de la carpeta ./images)
-# Configurar la propiedad source desde la raíz del proyecto (./images/<imagen.jpg>)
-#
-# Definir una regla de acceso para objeto creado de forma que sea público 
-# Configurar google_storage_object_access_control así
-#   role   = "READER"
-#   entity = "allUsers"
-# **********************  
+
+resource "google_storage_bucket" "imagenes_bucket" {
+  name                        = var.gcp_bucket_name
+  location                    = "europe-southwest1"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+
+  labels = {
+    owner    = var.gcp_username
+    proyecto = var.gcp_project
+  }
+}
+
+resource "google_storage_bucket_object" "imagen1" {
+  name   = "el_enigma_de_la_habitacion_622.jpg"
+  bucket = google_storage_bucket.imagenes_bucket.name
+  source = "${path.root}/images/el_enigma_de_la_habitacion_622.jpg"
+  content_type = "image/jpeg"
+}
+
+resource "google_storage_bucket_object" "imagen2" {
+  name   = "una_historia_de_espana.jpg"
+  bucket = google_storage_bucket.imagenes_bucket.name
+  source = "${path.root}/images/una_historia_de_espana.jpg"
+  content_type = "image/jpeg"
+}
+
+resource "google_storage_object_access_control" "public_imagen1" {
+  bucket = google_storage_bucket.imagenes_bucket.name
+  object = google_storage_bucket_object.imagen1.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_object_access_control" "public_imagen2" {
+  bucket = google_storage_bucket.imagenes_bucket.name
+  object = google_storage_bucket_object.imagen2.name
+  role   = "READER"
+  entity = "allUsers"
+}

50ce8eaa728a2b9832002fc5c332e8f1654f5336
jaimeparra2402 <jpj451@inlumine.ual.es>
2025-11-10 00:41:19 +0100

    Update variables.tf

--- Mixto/modules/Google-Storage/variables.tf
+++ Mixto/modules/Google-Storage/variables.tf
@@ -1,7 +1,17 @@
-# *** YOUR CODE HERE ***
-# Definir 3 variables
-# * gcp_username inicializada con el nombre de usuario en GCP
-# * gcp_project inicializada con el nombre del proyecto en GCP
-# * gcp_bucket_name inicializada con el nombre del bucket a crear en Google Storage
-# **********************
+variable "gcp_username" {
+  description = "Nombre de usuario en Google Cloud"
+  type        = string
+  default     = "jpj451" 
+}
 
+variable "gcp_project" {
+  description = "Nombre o ID del proyecto en Google Cloud"
+  type        = string
+  default     = "cc-25-26"  
+}
+
+variable "gcp_bucket_name" {
+  description = "Nombre del bucket en Google Cloud Storage"
+  type        = string
+  default     = "bucket"  
+}

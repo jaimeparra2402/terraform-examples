6d45ab5ed43dbc02a4ad0711ff4f29ef2be005f7
jaimeparra2402 <jpj451@inlumine.ual.es>
2025-11-10 18:25:17 +0100

    Update main.tf

--- Mixto/main.tf
+++ Mixto/main.tf
@@ -1,6 +1,4 @@
-module "Google-Storage" {
-  source          = "./modules/Google-Storage"
-  gcp_username    = "jpj451"
-  gcp_project     = "cc-25-26"
-  gcp_bucket_name = "bucket-jpj451-cc2526"
+module "OpenStack"{
+source = "./modules/OpenStack"
+PASSWORD="MII-2026"
 }

5f325e32107f29f0daa4444b6f5a2cfab518214f
jaimeparra2402 <jpj451@inlumine.ual.es>
2025-11-10 18:24:16 +0100

    Add files via upload

--- Mixto/modules/OpenStack/main.tf
+++ Mixto/modules/OpenStack/main.tf
@@ -1,67 +1,36 @@
-# *** YOUR CODE HERE ***
-# Crear instancia denominada mysql conectada a la red del proyecto e inicializada
-# con el archivo install_mysql.sh
-# Asignarle una dirección IP flotante
-# **********************
-
-# Configura el archivo de plantilla para la API
-data "template_file" "setup-api-docker" {
-  template = file("setup-api-docker.tpl")
-  vars = {
-    mysql_ip = openstack_compute_instance_v2.mysql.network.0.fixed_ip_v4
+# Crear nodo tf_vm
+resource "openstack_compute_instance_v2" "tf_vm" {
+  name              = "tf_vm"
+  image_name        = "jammy"
+  availability_zone = "nova"
+  flavor_name       = "medium"
+  key_pair          = var.openstack_keypair
+  security_groups   = ["default"]
+  network {
+    name = var.openstack_network_name 
   }
-  depends_on = [openstack_compute_instance_v2.mysql]
 }
 
-# Espera 5 minutos a que se configure la instancia MySQL
-# para que no falle el contenedor de la API al conectar a la BD
-resource "time_sleep" "wait_5_minutes" {
-  depends_on = [openstack_compute_instance_v2.mysql]
-
-  create_duration = "5m"
+# Crear una Floating IP
+resource "openstack_networking_floatingip_v2" "tf_vm_ip" { 
+  pool = "ext-net"
 }
 
-resource "openstack_compute_instance_v2" "book_api" {
-# *** YOUR CODE HERE ***
-# Configuración de la instancia denominada book_api 
-# conectada a la red del proyecto e inicializada 
-# con el archivo de la plantilla ya inicializado setup-api-docker
-# **********************
-
-  user_data = data.template_file.setup-api-docker.rendered
-
-  depends_on = [time_sleep.wait_5_minutes ]
-
+# Obtener el puerto de red asociado a la instancia
+data "openstack_networking_port_v2" "vm_port" {
+  device_id = openstack_compute_instance_v2.tf_vm.id
 }
 
-# *** YOUR CODE HERE ***
-# Asignarle una dirección IP flotante
-# **********************
-
-# Configura el archivo de plantilla para la aplicación
-data "template_file" "setup-app-docker" {
-  template = file("setup-app-docker.tpl")
-  vars = {
-    book_api_ip = openstack_compute_instance_v2.book_api.network.0.fixed_ip_v4
-  }
-  depends_on = [openstack_compute_instance_v2.book_api]
+# Asociar la Floating IP al puerto de la instancia
+resource "openstack_networking_floatingip_associate_v2" "tf_vm_ip" {
+  floating_ip = openstack_networking_floatingip_v2.tf_vm_ip.address
+  port_id     = data.openstack_networking_port_v2.vm_port.id
 }
 
-#Crear nodo APP
-resource "openstack_compute_instance_v2" "book_app" {
-# *** YOUR CODE HERE ***
-# Configuración de la instancia denominada book_app 
-# conectada a la red del proyecto e inicializada 
-# con el archivo de la aplicación ya inicializado setup-app-docker
-# **********************
-
-  user_data = data.template_file.setup-app-docker.rendered
+# Mostrar la Floating IP creada
+output "tf_vm_Floating_IP" {
+  value      = openstack_networking_floatingip_v2.tf_vm_ip.address 
+  depends_on = [openstack_networking_floatingip_v2.tf_vm_ip] 
 }
 
-# *** YOUR CODE HERE ***
-# Asignarle una dirección IP flotante
-# **********************
 
-# *** YOUR CODE HERE ***
-# Mostrar las direcciones IP generadas
-# **********************
--- Mixto/modules/OpenStack/variables.tf
+++ Mixto/modules/OpenStack/variables.tf
@@ -1,13 +1,32 @@
-# *** YOUR CODE HERE ***
-# Definir estas 4 variables
-# * openstack_user_name inicializada con el nombre de usuario en OpenStack
-# * openstack_tenant_name inicializada con el nombre del proyecto en OpenStack
-# * openstack_password inicializada con la contraseña en OpenStack
-# * openstack_keypair: Nombre del archivo de claves en OpenStack
+variable "openstack_user_name" {
+    description = "The username for the Tenant."
+    default  = "jpj451-MII"
+}
+
+variable "PASSWORD" {
+    description = "The user password."
+    type = string
+    sensitive = true
+}
+
+variable "openstack_tenant_name" {
+    description = "The name of the Tenant."
+    default  = "jpj451-jpj"
+}
 
 variable "openstack_auth_url" {
     description = "The endpoint url to connect to OpenStack."
-    default  = "http://openstack.di.ual.es:5000/v3"
+        default  = "https://192.168.64.50:5000/v3"
+}
+
+variable "openstack_keypair" {
+    description = "The keypair to be used."
+    default  = "jpj451-ual"
+}
+
+variable "openstack_network_name" {
+  description = "Nombre de la red a usar en OpenStack"
+  default     = "jpj451-net"
 }
 
 
